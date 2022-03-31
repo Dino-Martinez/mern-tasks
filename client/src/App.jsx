f9fd7567b9a1b3895a9e27137f3af18a39520dbe
Dino-Martinez <>
2022-03-30 20:34:42 -0400

    Built very basic Mantine app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,15 @@
 import './App.css'
-import React, { useEffect } from 'react'
-import useArray from './hooks/useArray'
-import useFetch from './hooks/useFetch'
+import React from 'react'
+import { Center, Container } from '@mantine/core'
+import TaskList from './components/TaskList'
+
 function App () {
-  const { arr, copy } = useArray([])
-  const { loading, value } = useFetch('/api') // Add error handling
-
-  useEffect(() => {
-    if (!loading && value) { copy(value.payload) }
-  }, [loading, value])
-
   return (
-    <div>
-      {!loading && value &&
-        <ul>
-          {arr.map(task => <li key={task._id}>{task.title} - {task.status}</li>)}
-        </ul>}
-    </div>
+    <Container size='sm'>
+      <Center>
+        <TaskList />
+      </Center>
+    </Container>
   )
 }
 
