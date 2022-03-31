36f6e06d240e65f093cebd25e9546982cc3b776a
Dino-Martinez <>
2022-03-30 22:36:51 -0400

    Exported individual tasks into their own component

---
+++ client/src/components/Task/Task.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Card, Title, Text } from '@mantine/core'
+import { shape, string, oneOf, instanceOf } from 'prop-types'
+import TaskIcon from '../TaskIcon/TaskIcon'
+import './Task.css'
+
+export default function Task ({ task }) {
+  return (
+    <Card shadow='md' p='lg'>
+      <Title component='span' order={2}><TaskIcon status={task.status} /> {task.title} - {task.status}</Title>
+      <Text size='xl'>{task.dueDate}</Text>
+    </Card>
+  )
+}
+
+Task.propTypes = {
+  task: shape({
+    status: oneOf(['complete', 'incomplete', 'in progress']),
+    title: string,
+    dueDate: instanceOf(Date)
+  })
+}
