d441140d256999cf32a8da1826e94f593abd8cef
Dino-Martinez <>
2022-03-30 23:45:24 -0400

    Fixed layout shifting on task cards with multiple lines

--- client/src/components/Task/Task.jsx
+++ client/src/components/Task/Task.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Card, Title, Text } from '@mantine/core'
+import { Card, Title, Text, Group, Stack } from '@mantine/core'
 import { shape, string, oneOf } from 'prop-types'
 import TaskIcon from '../TaskIcon/TaskIcon'
 import './Task.css'
@@ -7,8 +7,13 @@ import './Task.css'
 export default function Task ({ task }) {
   return (
     <Card shadow='md' p='lg'>
-      <Title component='span' order={2}><TaskIcon status={task.status} /> {task.title} - {task.status}</Title>
-      <Text size='xl'>{task.dueDate}</Text>
+      <Group>
+        <TaskIcon status={task.status} />
+        <Stack>
+          <Title component='span' order={2}> {task.title} - {task.status}</Title>
+          <Text size='xl'>{task.dueDate}</Text>
+        </Stack>
+      </Group>
     </Card>
   )
 }
