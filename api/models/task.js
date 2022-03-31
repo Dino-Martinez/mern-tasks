6f79851b699f7d6a2c45be0d455177f7abe549fa
Dino-Martinez <>
2022-03-30 22:25:41 -0400

    Added due date to tasks

--- api/models/task.js
+++ api/models/task.js
@@ -4,7 +4,8 @@ const { Schema } = mongoose
 
 const TaskSchema = new Schema({
   title: { type: String, required: true, maxlength: 60 },
-  status: { type: String, enum: ['incomplete', 'in progress', 'complete'], default: 'incomplete' }
+  status: { type: String, enum: ['incomplete', 'in progress', 'complete'], default: 'incomplete' },
+  dueDate: { type: Date, default: null }
 })
 
 module.exports = mongoose.model('Task', TaskSchema)
