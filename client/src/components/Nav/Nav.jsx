4c3cb4deb9a6cc02c33e7a26f521258bd6d9de66
Dino-Martinez <>
2022-03-30 23:22:24 -0400

    Set up theme toggle in header

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,12 +1,38 @@
-import { Header, Text, Title } from '@mantine/core'
+import { ActionIcon, Center, Group, Header, Title } from '@mantine/core'
+import { Sun, Moon } from 'tabler-icons-react'
 import React from 'react'
+import { func, oneOf } from 'prop-types'
 import './Nav.css'
 
-export default function Nav () {
+export default function Nav ({ theme, setTheme }) {
+  const toggleTheme = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light')
+  }
   return (
     <Header py='md' mb='lg'>
-      <Title order={1}>Simple Task Manager</Title>
-      <Text size='lg'>Built with the MERN Stack</Text>
+      <Group grow>
+        <Title order={1}>Simple Task Manager</Title>
+        <Group position='right'>
+          <Center>
+            <ActionIcon
+              variant='hover'
+              radius='lg'
+              size='xl'
+              onClick={toggleTheme}
+            >
+              {theme === 'dark' &&
+                <Sun size={32} />}
+              {theme === 'light' &&
+                <Moon size={32} />}
+            </ActionIcon>
+          </Center>
+        </Group>
+      </Group>
     </Header>
   )
+}
+
+Nav.propTypes = {
+  theme: oneOf(['dark', 'light']),
+  setTheme: func
 }
