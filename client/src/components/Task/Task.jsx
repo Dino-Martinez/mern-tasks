6b437531cceac5fb620b8f46564d3eb814f04d4c
Dino-Martinez <dino.martinez.514@gmail.com>
2022-04-06 01:18:45 -0400

    Implemented delete button to remove tasks from the list

--- client/src/components/Task/Task.jsx
+++ client/src/components/Task/Task.jsx
@@ -1,18 +1,25 @@
 import React from 'react'
 import { Card, Title, Text, Group, Stack } from '@mantine/core'
-import { shape, string, oneOf } from 'prop-types'
+import { shape, string, oneOf, func } from 'prop-types'
 import TaskIcon from '../TaskIcon/TaskIcon'
 import './Task.css'
+import DeleteIcon from '../DeleteIcon/DeleteIcon'
 
-export default function Task ({ task }) {
+export default function Task ({ task, onDelete }) {
+  const onClick = () => {
+    onDelete(task._id)
+  }
   return (
     <Card shadow='md' p='lg'>
-      <Group>
-        <TaskIcon status={task.status} />
-        <Stack>
-          <Title component='span' order={2}> {task.title} - {task.status}</Title>
-          <Text size='xl'>{task.dueDate}</Text>
-        </Stack>
+      <Group position='apart'>
+        <Group position='left'>
+          <TaskIcon status={task.status} />
+          <Stack>
+            <Title component='span' order={2}> {task.title}</Title>
+            <Text size='xl'>{task.dueDate}</Text>
+          </Stack>
+        </Group>
+        <DeleteIcon onClick={onClick} />
       </Group>
     </Card>
   )
@@ -23,5 +30,6 @@ Task.propTypes = {
     status: oneOf(['complete', 'incomplete', 'in progress']),
     title: string,
     dueDate: string
-  })
+  }),
+  onDelete: func
 }
--- client/src/components/TaskList/TaskList.jsx
+++ client/src/components/TaskList/TaskList.jsx
@@ -1,17 +1,18 @@
 import React, { useEffect, useState } from 'react'
 import useArray from '../../hooks/useArray'
 import useFetch from '../../hooks/useFetch'
-import { Button, Modal, Stack } from '@mantine/core'
+import { Button, Modal, Stack, Text } from '@mantine/core'
 import Task from '../Task/Task'
 import CreationForm from '../CreationForm/CreationForm'
 
 export default function TaskList () {
-  const { arr, copy, push } = useArray([])
-  const { data, loading, error, post } = useFetch('/api') // Add error handling
+  const { arr, copy, push, filter } = useArray([])
+  const { data, loading, error, post, get } = useFetch('/api') // Add error handling
   const [opened, setOpened] = useState(false)
-
   useEffect(() => {
-    if (!loading && data) { copy(data.payload) }
+    if (!loading && data) {
+      copy(data.payload)
+    }
   }, [loading, data])
 
   const addTask = async values => {
@@ -19,17 +20,23 @@ export default function TaskList () {
     push(result.payload)
   }
 
+  const removeTask = async id => {
+    const result = await get(`/${id}`, { method: 'DELETE' })
+    console.log(result)
+    if (result.payload.acknowledged) { filter(item => item._id !== id) }
+  }
+
   return (
     <>
       {error && 'Error'}
       {!loading && data &&
-        <Stack>
+        <Stack sx={{ width: '100%' }}>
           {arr.map(task => {
             return (
-              <Task key={task._id} task={task} />
+              <Task key={task._id} task={task} onDelete={removeTask} />
             )
           })}
-          <Button variant='gradient' gradient={{ from: 'indigo', to: 'cyan', deg: 45 }} onClick={() => setOpened(true)}>Create a Task</Button>
+          <Button variant='gradient' gradient={{ from: 'indigo', to: 'cyan', deg: 45 }} size='xl' onClick={() => setOpened(true)}> <Text size='xl'>Create a Task</Text> </Button>
           <Modal
             centered
             opened={opened}
