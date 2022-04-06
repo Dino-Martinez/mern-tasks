2ef7d7dd3c339aa63915214eb54c995871f62e21
Dino-Martinez <dino.martinez.514@gmail.com>
2022-04-06 00:13:52 -0400

    Refactored fetch hook to be compatible with manual fetching, dependency fetching, and declarative HTTP methods

--- client/src/hooks/useFetch.js
+++ client/src/hooks/useFetch.js
@@ -1,14 +1,42 @@
-import useAsync from './useAsync'
+import { useEffect, useRef, useState } from 'react'
 
 const DEFAULT_OPTIONS = {
-  headers: { 'Content-Type': 'application/json' },
+  headers: {
+    'Content-Type': 'application/json'
+  },
   method: 'GET'
 }
-const useFetch = (url, options = {}, dependencies = []) => {
-  return useAsync(async () => {
-    const res = await fetch(url, { ...DEFAULT_OPTIONS, ...options })
-    return res.ok ? res.json() : res.json().then(json => Promise.reject(json))
-  }, dependencies)
-}
 
-export default useFetch
+export default function useFetch (url, options = {}, dependencies = [], runOnMount = false) {
+  const [data, setData] = useState()
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState()
+  const firstUpdate = useRef(!runOnMount)
+
+  const get = (route = '', newOptions = {}) => {
+    const urlRoute = `${url}/${route}`
+    setLoading(true)
+    return fetch(urlRoute, { ...DEFAULT_OPTIONS, ...options, ...newOptions })
+      .then(res => res.json())
+      .then(json => {
+        setData(json)
+        setLoading(false)
+      }).catch(setError)
+  }
+
+  const post = (route = '', newOptions = {}) => {
+    const urlRoute = `${url}/${route}`
+    return fetch(urlRoute, { ...DEFAULT_OPTIONS, ...options, ...newOptions, method: 'POST' })
+      .then(res => res.json())
+      .then(json => {
+        return json
+      }).catch(setError)
+  }
+
+  useEffect(() => {
+    if (!firstUpdate.current) get()
+    if (firstUpdate.current) firstUpdate.current = false
+  }, dependencies)
+
+  return { data, loading, error, get, post }
+}
